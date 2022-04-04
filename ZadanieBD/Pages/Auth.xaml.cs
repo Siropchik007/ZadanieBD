9ada7160ee1d0b5f6add2d6c31dd9ab8e590ecc5
Siropchik007 <stas.koval.89@mail.ru>
2022-04-04 09:31:47 +0300

    Добавьте файлы проекта.

---
+++ ZadanieBD/Pages/Auth.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZadanieBD.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Auth.xaml
+    /// </summary>
+    public partial class Auth : Page
+    {
+        public Auth()
+        {
+            InitializeComponent();
+        }
+
+        private void Auth_Click(object sender, RoutedEventArgs e)
+        {
+            if(login.Text =="admin" & password.Text == "admin"){
+                MainWindow mainWindow = Window.GetWindow(this) as MainWindow;
+                mainWindow.FrmMain.Navigate(new Pages.Client());
+            }
+            else
+            {
+                MessageBox.Show("Попробуйте ещё раз", "Неверные данные", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
